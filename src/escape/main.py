032ea41d80b3d1fd9ba6f19630e035e96b2592a6
Rebecca Chen <rechen@google.com>
2019-09-24 21:53:03 -0700

    Add an option to skip the title card.

--- src/escape/main.py
+++ src/escape/main.py
@@ -1,13 +1,22 @@
 """Main entrypoint."""
+import argparse
 import pygame
 from . import state
+
+
+def args():
+    parser = argparse.ArgumentParser(description='kitty escape game')
+    parser.add_argument('-s', '--skip-title', action='store_true',
+                        default=False, help='skip the title card')
+    return parser.parse_args()
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode(state.WINSIZE)
     pygame.display.set_caption('Kitty Escape')
-    state.TitleCard(screen).run()
+    if not args().skip_title:
+        state.TitleCard(screen).run()
     state.Game(screen).run()
 
 
